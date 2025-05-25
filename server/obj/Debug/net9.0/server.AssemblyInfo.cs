d925584d597a97d50fc98d5de73e2cbf7a05400f
Tate, Thane <thanetate@my.unt.edu>
2025-05-25 13:45:50 -0500

    Init

--- server/obj/Debug/net9.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net9.0/server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+419d3af45820c4e1d716caac1c3d59bdf9919cac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6c26e9f7046df8629e4a46fd79fa11a78141f18")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
